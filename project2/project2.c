031aa4e7b0ef1e6945a5281fdcda72b770addb8f
Goriee <jedevela@gbox.ncf.edu.ph>
2024-08-28 09:55:08 +0800

    first commit

---
+++ project2/project2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+
+int myvar1;
+myvar1 = 10;
+
+int myvar2;
+myvar2 = 50;
+
+int total;
+total = myvar1 + myvar2;
+
+
+printf("first number: %d\n", myvar1 );
+printf("second number: %d\n",myvar2 );
+printf("total: %d\n", total);
+
+return 0;
+}
