89ecfa2fd0ffe719185ddb98880d57cd68598505
Goriee <jedevela@gbox.ncf.edu.ph>
2024-09-09 08:44:25 +0800

    third commit prelim act

---
+++ project3/premlimtest2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+    int num1,num2;
+
+
+    scanf("%d", &num1);
+    scanf("%d", &num2);
+    float result = num1 + num2;
+    printf("%f",result);
+
+    return 0;
+
+}
