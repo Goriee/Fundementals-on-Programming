7bbf7ae39b5a4588db403c6fd5c241f409b037b6
Goriee <jedevela@gbox.ncf.edu.ph>
2024-09-25 08:48:01 +0800

    fifth commit

---
+++ project5/project5.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main (){
+
+
+printf("----- ***** ----- ***** ----- ***** -----\nWelcome to Naga College Foundation\nI am Jerome De Vela from the\nCollege of Computer Studies\n***** ----- ***** ----- ***** ----- *****");
+
+return 0;
+
+}
