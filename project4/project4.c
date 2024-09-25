6b25d4075704ba09ce7f8f923a1e2904426ddaf4
Goriee <jedevela@gbox.ncf.edu.ph>
2024-09-25 08:47:09 +0800

    project4

---
+++ project4/project4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main (){
+
+
+    int intvrar = 2;
+    float floatvar = 4.1;
+    char charvar = '1';
+    char string[] = "string";
+
+    printf("%d\n", intvrar);
+    printf("%f\n",floatvar);
+    printf("%c\n", charvar);
+    printf("%s\n", string);
+    
+
+}
