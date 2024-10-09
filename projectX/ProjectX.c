80ac9e0e485f99d5a85c0cd9af9b56a08d5ff50d
Goriee <jedevela@gbox.ncf.edu.ph>
2024-10-09 09:46:02 +0800

    simple else if

---
+++ projectX/ProjectX.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+
+
+int var = 1;
+
+
+while(var != 0){
+
+printf("Enter a number: ");
+scanf("%d", &var);
+    if (var == 5){
+        printf("success\n");
+        
+        return 0;
+
+    }
+    else(printf("failure\n"));
+    
+    if (var == 0)
+    {
+        printf("Exiting program\n");
+    }
+    
+
+}
+
+
+    return 0;
+}
